83680bb7003f470e0431e2dbdb589b8b757f2449
PhalanxHead <lhedt@hotmail.com>
2021-02-19 20:21:58 +1100

    Add project files.

---
+++ ToyRobotChallenge/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ToyRobotChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
